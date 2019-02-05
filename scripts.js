ef397e2487024d577e5ac3ec8eb5311e515932c0
Aubrey Stento <aubreystento@gmail.com>
2019-02-04 18:54:45 -0700

    lock board

--- scripts.js
+++ scripts.js
@@ -1,7 +1,46 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+// prevents more than two cards being revealed
+let lockBoard = false;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    if (lockBoard) return;
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+
+    }
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
 }
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        lockBoard = false;
+    }, 1500);
+}
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
